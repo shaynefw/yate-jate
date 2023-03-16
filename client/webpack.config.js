9c0e3cf0cda022f34f290252a8899b0955664d3b
swmac <shaynefw@gmail.com>
2023-03-16 09:58:04 -0400

    feat: added plugins to webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,47 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        filename: 'index.html',
+        inject: 'head',
+        scriptLoading: 'defer',
+      }),
+      new WebpackPwaManifest({
+        name: 'J.A.T.E.',
+        short_name: 'JATE',
+        description: 'Just Another Text Editor',
+        background_color: '#01579b',
+        crossorigin: 'use-credentials',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
+    ],
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
